70d5053dc1404976863289f9258621e9769e4fec
viswa3047 <viswa3047@gmail.com>
2024-09-07 17:18:06 +0530

    updated

---
+++ run_finance_tracker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/Viswa7204/Personal-Finance-Tracker.git
+
+# Navigate into the repository directory
+cd Personal-Finance-Tracker
+
+# Install required packages
+pip install -r requirements.txt
+
+# Run the main Python script
+python main.py
